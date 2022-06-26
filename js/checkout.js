3bbe5afc1136754d07bfbf248f0df1d49d308edd
David Gomez Muns <davidmuns@yahoo.es>
2022-06-26 11:54:10 +0200

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,4 @@
 // Exercise 7
-
 const form = document.getElementById('form');
 const inputs = document.querySelectorAll('#form input');
 const regex = {
@@ -11,7 +10,7 @@ const regex = {
     address: /^[a-zA-Z0-9\s]{3,25}$/
 }
 
-const campos = {
+const fields = {
     firstName: false,
     lastName: false,
     phoneNumber: false,
@@ -24,55 +23,56 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
 });
 
-const validarCampos = function (expresion, input, campo, error) {
-    if (expresion.test(input.value)) {
-        document.getElementById(`group-${campo}`).classList.add('valid-form');
-        document.getElementById(`group-${campo}`).classList.remove('invalid-form');
-        document.querySelector(`#group-${campo} i`).classList.add('fa-check-circle');
-        document.querySelector(`#group-${campo} i`).classList.remove('fa-times-circle');
+const validateField = function (expression, input, field, error) {
+    if (expression.test(input.value)) {
+        document.getElementById(`${field}`).classList.add('valid-form');
+        document.getElementById(`${field}`).classList.remove('invalid-form');
+        document.querySelector(`#${field} i`).classList.add('fa-check-circle');
+        document.querySelector(`#${field} i`).classList.remove('fa-times-circle');
         document.getElementById(`${error}`).classList.add('invalid-feedback');
-        campos[campo] = true;
+        fields[field] = true;
 
     } else {
-        document.getElementById(`group-${campo}`).classList.add('invalid-form');
-        document.getElementById(`group-${campo}`).classList.remove('valid-form');
-        document.querySelector(`#group-${campo} i`).classList.remove('fa-check-circle');
-        document.querySelector(`#group-${campo} i`).classList.add('fa-times-circle');
+        document.getElementById(`${field}`).classList.add('invalid-form');
+        document.getElementById(`${field}`).classList.remove('valid-form');
+        document.querySelector(`#${field} i`).classList.remove('fa-check-circle');
+        document.querySelector(`#${field} i`).classList.add('fa-times-circle');
         document.getElementById(`${error}`).classList.remove('invalid-feedback');
-        campos[campo] = false;
+        fields[field] = false;
     }
 };
 
-const validarForm = function (e) {
+const validateForm = function (e) {
 
     switch (e.target.name) {
         case 'firstName':
-            validarCampos(regex.firstName, e.target, e.target.name, 'errorName');
+            validateField(regex.firstName, e.target, e.target.name, 'errorName');
             break;
         case 'email':
-            validarCampos(regex.email, e.target, e.target.name, 'errorEmail');
+            validateField(regex.email, e.target, e.target.name, 'errorEmail');
             break;
         case 'address':
-            validarCampos(regex.address, e.target, e.target.name, 'errorAddress');
+            validateField(regex.address, e.target, e.target.name, 'errorAddress');
             break;
         case 'password':
-            validarCampos(regex.password, e.target, e.target.name, 'errorPassword');
+            validateField(regex.password, e.target, e.target.name, 'errorPassword');
             break;
         case 'lastName':
-            validarCampos(regex.lastName, e.target, e.target.name, 'errorLastN');
+            validateField(regex.lastName, e.target, e.target.name, 'errorLastN');
             break;
         case 'phoneNumber':
-            validarCampos(regex.phoneNumber, e.target, e.target.name, 'errorPhone');
+            validateField(regex.phoneNumber, e.target, e.target.name, 'errorPhone');
             break;
     }
 }
 
 for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keyup', validarForm);
+    inputs[i].addEventListener('keyup', validateForm);
 }
 
-function validate() {
-    if (campos.firstName && campos.lastName && campos.password && campos.phoneNumber && campos.address && campos.email) {
+function validateSubmit() {
+
+    if (fields.firstName && fields.lastName && fields.password && fields.phoneNumber && fields.address && fields.email) {
         form.reset();
         let validForm = document.querySelectorAll('.valid-form');
 
@@ -81,16 +81,17 @@ function validate() {
             validForm[i].classList.remove('valid-form');
 
         }
-        campos.firstName = false;
-        campos.lastName = false;
-        campos.password = false;
-        campos.phoneNumber = false;
-        campos.address = false;
-        campos.email = false;
+
+        fields.firstName = false;
+        fields.lastName = false;
+        fields.password = false;
+        fields.phoneNumber = false;
+        fields.address = false;
+        fields.email = false;
 
         alert("Form submitted");
     } else {
-        alert('Fill in all the fields of the form');
+        alert('Fill correctly in all the fields of the form');
     }
 }
 
