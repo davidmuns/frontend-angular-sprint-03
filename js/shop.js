cd2f5857328a03fa0b80d3ee8e7d04862a257e99
David Gomez Muns <davidmuns@yahoo.es>
2022-06-26 19:29:12 +0200

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,79 +1,85 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+var products = [];
+
+const requestURL = '../json/products.json';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    products = request.response;
+}
+//var products = [
+//     {
+//         id: 1,
+//         name: 'cooking oil',
+//         price: 10.5,
+//         type: 'grocery',
+//         offer: {
+//             number: 3,
+//             percent: 20
+//         }
+//     },
+//     {
+//         id: 2,
+//         name: 'Pasta',
+//         price: 6.25,
+//         type: 'grocery'
+//     },
+//     {
+//         id: 3,
+//         name: 'Instant cupcake mixture',
+//         price: 5,
+//         type: 'grocery',
+//         offer: {
+//             number: 10,
+//             percent: 30
+//         }
+//     },
+//     {
+//         id: 4,
+//         name: 'All-in-one',
+//         price: 260,
+//         type: 'beauty'
+//     },
+//     {
+//         id: 5,
+//         name: 'Zero Make-up Kit',
+//         price: 20.5,
+//         type: 'beauty'
+//     },
+//     {
+//         id: 6,
+//         name: 'Lip Tints',
+//         price: 12.75,
+//         type: 'beauty'
+//     },
+//     {
+//         id: 7,
+//         name: 'Lawn Dress',
+//         price: 15,
+//         type: 'clothes'
+//     },
+//     {
+//         id: 8,
+//         name: 'Lawn-Chiffon Combo',
+//         price: 19.99,
+//         type: 'clothes'
+//     },
+//     {
+//         id: 9,
+//         name: 'Toddler Frock',
+//         price: 9.99,
+//         type: 'clothes'
+//     }
+// ]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-var discount = 0;
 var total;
 var accItems = 0;
-var totalWithoutDiscount;
-
-
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
@@ -88,18 +94,15 @@ function buy(id) {
     // cartList.push(item);
     // generateCart();
     // document.getElementById('count_product').innerHTML = ++accItems;
-
-
 }
 
 // Exercise 2
 function cleanCart() {
     total = 0;
-    discount = 0;
     accItems = 0;
     cartList = [];
     cart = [];
-    document.getElementById("total_price").innerHTML = 0;
+    document.getElementById("total_price").innerHTML = "0.00";
     document.getElementById("cart_list").innerHTML = "";
     document.getElementById("count_product").innerHTML = 0;
 }
@@ -108,13 +111,9 @@ function cleanCart() {
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
     total = 0;
-    totalWithoutDiscount = 0;
     for (let i = 0; i < cart.length; i++) {
-        totalWithoutDiscount += cart[i].subtotal;
         total += cart[i].subtotalWithDiscout;
     }
-    discount = totalWithoutDiscount - total;
-    console.log(discount);
     return total;
 }
 
@@ -163,6 +162,7 @@ function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
     const tableTag = document.getElementById('cart_list');
     tableTag.innerHTML = '';
+
     for (let i = 0; i < cart.length; i++) {
         const table = document.createElement('tr');
 
@@ -187,8 +187,6 @@ function printCart() {
         table.appendChild(itemQuantity);
         table.appendChild(subtotal);
         table.appendChild(subtotalWithDiscout);
-
-
     }
 }
 
